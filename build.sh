e612f3253a8c8d423327d3660e0422e06663c02c
ReyAcosta <acostalandarey@gmail.com>
2025-11-11 16:40:55 -0600

    agregar super user

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+DJANGO_SUPERUSER_USERNAME=dharm \
+DJANGO_SUPERUSER_EMAIL=acostalandarey@gmail.com \
+DJANGO_SUPERUSER_PASSWORD=SuperUserPass1984 \
+python manage.py createsuperuser --noinput
